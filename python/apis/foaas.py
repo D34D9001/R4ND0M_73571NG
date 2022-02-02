b0a7e9bc5a19ebf49e8c9bdc541464a2248d19e4
73RM1N41 <t3rm1n41@localhost.localdomain>
2022-02-01 19:44:33 -0600

    Minor updates to foaas.py

--- python/apis/foaas.py
+++ python/apis/foaas.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""This program is still a little bit buggy.
+Feel free to imporove it!
+"""
+
 __author__ = "D34D9001"
 __license__ = "MIT"
 __version__ = "1.0.1"
@@ -16,27 +20,28 @@ from termcolor import colored
 argv = sys.argv[1:]
 
 name = ""
-sender = ""
+_from = ""
+company = ""
 method = ""
 
 try:
-    total = len(sys.argv)
-    if int(total) < 4:
-        print("[Invalid] Please Try Again\nfoaas.py -m [method] -s [sender] -n [name]")
-        sys.exit()
-    else:
-        options, args = getopt.getopt(argv, "m:n:s:h",
-                                   ["method=",
-                                    "sender=",
-                                    "name=",
-                                    "help"])
+
+    options, args = getopt.getopt(argv, "m:f:c:n:h",
+                               ["method=",
+                                "from=",
+                                "company",
+                                "name=",
+                                "help"])
 
     for name, value in options:
         if name in ['-m', '--method']:
             method = value
 
-        if name in ['-s', '--sender']:
-            sender = value
+        if name in ['-f', '--from']:
+            _from = value
+
+        if name in ['-c', '--company']:
+            company = value
 
         if name in ['-n', '--name']:
             name = value
@@ -47,11 +52,7 @@ try:
 
     try:
 
-        if len(name) < 1:
-            print("[!] You must provide a valid name.")
-            sys.exit()
-
-        elif len(sender) < 1:
+        if len(_from) < 1:
             print("[*] You Must Specify A Sender....")
             sys.exit()
 
@@ -59,11 +60,27 @@ try:
             print("[!] You must choose a method...")
             sys.exit()
 
-        headers = {"User-Agent": "FOaaS"}
-        data = requests.get("https://foaas.com/%s/%s/%s" % (method, name, sender), headers=headers)
-        tree = fromstring(data.content)
-        fuckoff = tree.findtext('.//title')
-        print(fuckoff)
+
+        elif len(name) <= 2:
+            headers = {"User-Agent": "FOaaS"}
+            data = requests.get("https://foaas.com/%s/%s/" % (method, _from), headers=headers)
+            tree = fromstring(data.content)
+            fuckoff = tree.findtext('.//title')
+            print(fuckoff)
+
+        elif len(company) >= 2:
+            headers = {"User-Agent": "FOaaS"}
+            data = requests.get("https://foaas.com/%s/%s/%s/" % (method, company, _from), headers=headers)
+            tree = fromstring(data.content)
+            fuckoff = tree.findtext('.//title')
+            print(fuckoff)
+
+        else:
+            headers = {"User-Agent": "FOaaS"}
+            data = requests.get("https://foaas.com/%s/%s/%s" % (method, name, _from), headers=headers)
+            tree = fromstring(data.content)
+            fuckoff = tree.findtext('.//title')
+            print(fuckoff)
 
     except Exception as error:
         print(error)
